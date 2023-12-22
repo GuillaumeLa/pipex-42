6ad4c3edde6542f9900cad9a7818175fdeb0b199
Guillaume Labadens D'Arc <glabaden@made-f0Ar8s12.clusters.42paris.fr>
2023-12-22 08:43:50 +0100

    pomme

--- includes/pipex.h
+++ includes/pipex.h
@@ -4,6 +4,9 @@
 # include <fcntl.h>
 # include <unistd.h>
 # include <sys/wait.h>
+# include <stdio.h>
 
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strnstr(const char *str, const char *to_find, size_t n);
 char	**ft_split(char const *s, char c);
 #endif
